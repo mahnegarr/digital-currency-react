454002a1ed608e6279cf2fe56091330a7c4ff673
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-05 18:42:01 +0330

    #

--- src/components/templates/Homepage.jsx
+++ src/components/templates/Homepage.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Tablecoin from "../modules/Tablecoin"
+import Tablecoin from "../modules/Tablecoin";
+import getCoinList from "../../services/cryptoAPI";
 
 function Homepage() {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=2&sparkline=false&locale=en&x_cg_demo_api_key=CG-QhShH2Lb5wntC2i6yqhZcmj4"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+
+    getData();
   }, []);
   return (
     <div>
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,7 @@
+const BASE_URL = "https://api.coingecko.com/api";
+const API_KEY = "CG-QhShH2Lb5wntC2i6yqhZcmj4";
+const getCoinList = () => {
+  return `${BASE_URL}/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=2&sparkline=false&locale=en&x_cg_demo_api_key=${API_KEY}`;
+};
+
+export default getCoinList ;
