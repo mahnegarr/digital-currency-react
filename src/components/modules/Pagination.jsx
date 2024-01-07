c7b6f59f0c3d270350a126cabc878feb9ddb2e5a
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-07 18:35:23 +0330

    #

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
+import styles from "../modules/Pagination.module.css";
 
-function Pagination() {
-  const [page, setPage] = useState(1);
-
+function Pagination({ page, setPage }) {
   const previousHandler = () => {
-    setPage((page) => (page == 1 ? 1 : page-1));
+    setPage((page) => (page == 1 ? 1 : page - 1));
   };
   const nextHandler = () => {
-    setPage((page) => page>=10 ? 10: page + 1);
+    setPage((page) => (page >= 10 ? 10 : page + 1));
   };
   return (
-    <div>
-      <button onClick={previousHandler}>Previous</button>
-      <p>{page}</p>
-      <button onClick={nextHandler}>Next</button>
+    <div className={styles.paginationDiv}>
+      <button
+        onClick={previousHandler}
+        className={page === 1 ? styles.disabled : styles.button}
+      >
+        Previous
+      </button>
+      <p className={page ===1 ? styles.selected : null}>1</p>
+      <p className={page ===2 ? styles.selected : null}>2</p>
+      {page > 2 && page < 9 && (
+        <>
+          <span>...</span>
+          <p className={styles.selected}>{page}</p>
+        </>
+      )}
+      <span>...</span>
+      <p className={page ===9 ? styles.selected : null}>9</p>
+      <p className={page ===10 ? styles.selected : null}>10</p>
+      <button onClick={nextHandler} className={page === 10 ? styles.disabled:styles.button}>
+        Next
+      </button>
     </div>
   );
 }
--- src/components/templates/Homepage.jsx
+++ src/components/templates/Homepage.jsx
@@ -6,21 +6,23 @@ import Pagination from "../modules/Pagination";
 function Homepage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true)
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
-      setIsLoading(false)
+      setIsLoading(false);
     };
 
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
-      <Pagination />
       <Tablecoin coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }
