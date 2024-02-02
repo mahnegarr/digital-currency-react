1942902034919cd908bef45b1ad6703384ea9981
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-02-02 18:42:46 +0330

    #

---
+++ src/components/modules/TableRow.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
+import style from "../modules/Tablecoin.module.css";
+
+function TableRow({
+  coin: {
+    image,
+    symbol,
+    name,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change,
+  },
+}) {
+  return (
+    <tr>
+      <td>
+        <div className={style.symbol}>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>{current_price.toLocaleString()}</td>
+      <td className={price_change > 0 ? style.success : style.error}>
+        {price_change.toFixed(2)}
+      </td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? chartUp : chartDown} alt="" />
+      </td>
+    </tr>
+  );
+}
+
+export default TableRow;
--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,44 +1,31 @@
 import React from "react";
-import chartUp from "../../assets/chart-up.svg";
-import chartDown from "../../assets/chart-down.svg";
-import { RotatingLines } from "react-loader-spinner";
-import styles from "../modules/Tablecoin.module.css";
-import { tr } from "date-fns/locale";
+import TableRow from "./TableRow";
+import {RotatingLines} from 'react-loader-spinner'
+import style from "../modules/Tablecoin.module.css"
 
-function Tablecoin({ coins }) {
-  console.log(coins);
+function Tablecoin({ coins, isLoading }) {
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volumes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <tr key={coin.id}>
-              <td>
-                <div>
-                  <img src={coin.image} alt="" />
-                  <span>{coin.symbol.toUpperCase()}</span>
-                </div>
-              </td>
-              <td>{coin.name}</td>
-              <td>{coin.current_price.toLocaleString()}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}</td>
-              <td>{coin.total_volume.toLocaleString()}</td>
-              <td>
-                {coin.price_change_percentage_24h > 0 ? chartUp : chartDown}
-              </td>
+    <div className={style.mainDiv}>
+      {isLoading ? (
+        <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volumes</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
