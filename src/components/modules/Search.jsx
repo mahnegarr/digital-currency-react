be61a838b4a8063aeb7b2e8f7058b4340d1de32a
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-02-02 22:04:26 +0330

    #

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,76 +1,32 @@
-// import React, { useEffect, useState } from "react";
-// import search from "../../services/cryptoAPI.js";
-// import { RotatingLines } from "react-loader-spinner";
+import React, { useEffect, useState } from "react";
 
-// function Search({ currency, setCurrency }) {
-//   const [text, setText] = useState("");
-//   const [coins, setCoins] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
+function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    if (!text) return;
+    const search = async () => {
+      const res = await fetch(searchCoin(text));
+      const json = await res.json();
+      console.log(json);
+      setCoins(json.coins)
+    };
+  }, [text]);
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search "
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+        <option value="usd">USD</option>
+        <option value="eur">EUR</option>
+        <option value="jpy">JPY</option>
+      </select>
+    </div>
+  );
+}
 
-//   useEffect(() => {
-//     const controller = new AbortController();
-
-//     setCoins([]);
-
-//     if (!text) return;
-//     const searchBox = async () => {
-//       try {
-//         const res = await fetch(search.search(text), {
-//           signal: controller.signal,
-//         });
-//         const json = await res.json();
-//         if (json.coins) {
-//             setCoins(json.coins);
-//             setIsLoading(false);
-//         } else {
-//           alert(json.status.error_message);
-//         }
-//       } catch (error) {
-//         if (error.name != "AbortError") {
-//           alert(error.message);
-//         }
-//       }
-//     };
-
-//     setIsLoading(true);
-//     searchBox();
-
-//     return () => controller.abort();
-//   }, [text]);
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="Search"
-//         value={text}
-//         onChange={(e) => setText(e.target.value)}
-//       />
-//       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
-//         <option value="usd">USD</option>
-//         <option value="eur">EUR</option>
-//         <option value="jpy">JPY</option>
-//       </select>
-
-//       <div>
-//         {isLoading && (
-//           <RotatingLines
-//             width="50px"
-//             height="50px"
-//             strokeColor="#3874ff"
-//             strokeWidth="2"
-//           />
-//         )}
-//         <ul>
-//           {coins.map((coin) => (
-//             <li key={coin.id}>
-//               <img src={coin.thumb} alt={coin.name} />
-//               <p>{coin.name}</p>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Search;
+export default Search;
--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,6 +1,12 @@
-const BASE_URL = "https://api.coingecko.com/api";
+const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-jvGLYx23RWizTMRcGsj88SGK";
 
-export const getCoinList = (page) => {
-  return `${BASE_URL}/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
+export const getCoinList = (page, currency) => {
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 };
+
+const searchCoin = (query) => {
+  return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+};
+
+export default { getCoinList, searchCoin };
