8eadfe492b918e809ae6cd5dc6e3bc4dce699bc4
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-02-26 12:10:22 +0330

    #

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function layout({children}) {
+  return <>
+    <header>
+      <h1>Crypto App</h1>
+      <p><a href="https://github.com/mahnegarr?tab=overview&from=2024-02-01&to=2024-02">My Github</a></p>
+    </header>
+    {children}
+    <footer>
+        <p>Developed by Mahnegar with ❤️ </p>
+    </footer>
+  </>
+  
+}
+
+export default layout;
